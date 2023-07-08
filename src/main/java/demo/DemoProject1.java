a6b501f51652f232b7d1a5e2bc0cd39c978f4e1f
NikhilPDeshmukh <nikhil.deshmukh219563@gmail.com>
2023-07-08 14:37:35 +0530

    [NIKHIL DESHMUKH] NIKHIL ADDED NEW DEMO CLASS

---
+++ src/main/java/demo/DemoProject1.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class DemoProject1 {
+
+	public static void main(String[] args)
+	{
+		String S1 = "NIKHIL JAYPRAKASHRAO DESHMUKH";
+		String S2 = "BE MECHANICAL 2016 FROM AMRAVATI";
+		System.out.println(S1);
+		System.out.println(S2);
+
+	}
+
+}
